019dc098d8c894366fd85c9e394af7f0860aed6f
Brian Haas <brianjohnhaas@gmail.com>
2017-06-21 10:52:57 -0400

    add

---
+++ _tests/test.genedb.reader.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+
+
+
+import sys, os
+import haasbio
+from haasbio.gtf.gtf_reader import *
+
+import shelve
+
+d = shelve.open("genes")
+
+gene_ids = d.keys()
+
+for gene_id in gene_ids:
+    print(gene_id)
+
+d.close()
+
+
+
+
---
+++ _tests/test.haasbio.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from haasbio import *
+
+print(Gene('x'))
+
